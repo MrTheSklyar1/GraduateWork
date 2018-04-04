5c7dbc49505e660a6aa4362b4c40909373a68742
Nikita Sklyarov <thesklyar@gmail.com>
2018-04-05 00:07:31 +0300

    Переделан процесс присваивания версий приложениям, внесена правка в клиент

--- Source/ClientApp/BaseClasses/PDFSigner.cs
+++ Source/ClientApp/BaseClasses/PDFSigner.cs
@@ -30,7 +30,7 @@ namespace ClientApp.BaseClasses
                     catch
                     {
                         EnvironmentHelper.SendDialogBox(
-                            (string)SystemSingleton.Configuration.mainWindow.FindResource("m_CertPassError") + (i - 1),
+                            (string)SystemSingleton.Configuration.mainWindow.FindResource("m_CertPassError") +" "+ (i - 1),
                             "Certificate/Password Error"
                         );
                         SystemSingleton.CurrentSession.CertPassword = "";
